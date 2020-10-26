091bd85c3b6c2241753ccabbbb7fd1e3819b6b00
CraigSmith789 <craig789@gmail.com>
2020-10-26 18:18:52 -0400

    commit tree f9aa947c33b134885f5d8c1254f4806ead48b8c4

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,18 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
 
+def key_for_min_value(name_hash)
+    stash_array = []
+    name_hash.map do | key, value |
+        stash_array.push (value)
+    end
+        tester = stash_array.shift
+        stash_array.each do |item|
+            if item < tester
+                tester = item
+            end
+        end      
+        
+    name_hash.key(tester)
 end
